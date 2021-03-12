d8effa703580cfe5a7c476a812410a4dcff15063
asaez <alex.saez.12@gmail.com>
2021-03-12 17:37:40 +0100

    TEST: test aero to body and angular kinematic eqs

--- test/kinematics.jl
+++ test/kinematics.jl
@@ -62,3 +62,38 @@ using Test
     accel_Q = rigid_body_acceleration(acc_P, ω, ω_dot, r_PQ)
     @test isapprox(accel_Q, exp_accel_Q)
 end
+
+
+@testset "Angular kinematic equations" begin
+    # Null Euler angles
+    pqr = [0.5, 0.3, 0.7]
+    euler_angles_rates = body_angular_velocity_to_euler_angles_rates(pqr..., 0, 0)
+    @test isapprox(euler_angles_rates, pqr[end:-1:1])
+
+    ψθϕ_dot = [0.5, 0.3, 0.7]
+    pqr_ = euler_angles_rates_to_body_angular_velocity(ψθϕ_dot..., 0, 0)
+    @test isapprox(pqr_, ψθϕ_dot[end:-1:1])
+
+    # Reciprocal relationships
+    θ = 0.15  # rad
+    ϕ = 0.6  # rad
+    pqr = [0.5, 0.3, 0.7]
+    euler_angles_rates = body_angular_velocity_to_euler_angles_rates(pqr..., θ, ϕ)
+    pqr_ = euler_angles_rates_to_body_angular_velocity(euler_angles_rates..., θ, ϕ)
+    @test isapprox(pqr, pqr_)
+
+    # TODO: test body_angular_velocity_to_quaternion_rates
+end
+
+
+@testset "Aero <--> Body velocity" begin
+    uvw = [30, 3, 5]
+    tas, α, β = uvw_to_tasαβ(30, 3, 5)
+    u, v, w = wind2body(tas, 0, 0, α, β)
+    @test isapprox([u, v, w], uvw)
+
+    uvw_dot = [5, 1, 10]
+    tasαβ_dot = tasαβ_dot_from_uvw_dot(uvw..., uvw_dot...)
+    uvw_dot_ = uvw_dot_from_tasαβ_dot(uvw_to_tasαβ(uvw...)..., tasαβ_dot...)
+    @test isapprox(uvw_dot_, uvw_dot)
+end
