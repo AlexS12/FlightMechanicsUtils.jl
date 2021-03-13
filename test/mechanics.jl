326d2bbfcfbfef632b73c62bd673137d2c2f69b7
asaez <alex.saez.12@gmail.com>
2021-03-13 13:05:55 +0100

    TEST: add more tests for forces moments translation

--- test/mechanics.jl
+++ test/mechanics.jl
@@ -3,21 +3,29 @@ using Test
 
 
 @testset "Translate forces and moments" begin
-    f1 = [10, 20, 30]
-    m1 = [50, 60, 90]
-    p1 = [1, 1, 1]
-
     # No traslation
-    m2 = translate_forces_moments(f1, m1, p1, p1)
-    @test isapprox(m2, m1)
+    m2 = translate_forces_moments([10, 20, 30], [50, 60, 90], [1, 1, 1], [1, 1, 1])
+    @test isapprox(m2, [50, 60, 90])
 
-    # Simple translation
-    m2 = translate_forces_moments(f1, m1, p1, p1 + [0, 1, 0])
-    @test isapprox(m2, m1 + [-30, 0, 10])
+    # Simple translation 1
+    m2 = translate_forces_moments([1, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 1])
+    @test isapprox(m2, [0, -1, 0])
+
+    # Simple translation 2
+    m2 = translate_forces_moments([1, 0, 0], [1, 0, 0], [0, 0, 0], [0, 0, 1])
+    @test isapprox(m2, [1, -1, 0])
+
+    # Simple translation 3
+    m2 = translate_forces_moments(
+        [10, 20, 30],
+        [50, 60, 90],
+        [1, 1, 1],
+        [1, 1, 1] + [0, 1, 0],
+    )
+    @test isapprox(m2, [50, 60, 90] + [-30, 0, 10])
 
     # Reciprocal
-    p2 = [10, 20, 30]
-    m2 = translate_forces_moments(f1, m1, p1, p2)
-    m1_ = translate_forces_moments(f1, m2, p2, p1)
-    @test isapprox(m1, m1_)
+    m2 = translate_forces_moments([10, 20, 30], [50, 60, 90], [1, 1, 1], [10, 20, 30])
+    m1_ = translate_forces_moments([10, 20, 30], m2, [10, 20, 30], [1, 1, 1])
+    @test isapprox([50, 60, 90], m1_)
 end
